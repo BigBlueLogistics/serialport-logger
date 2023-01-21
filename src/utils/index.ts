808b6a6514660d38c90f9bea50584e1469999441
Roelan Geil <roelaneroy@gmail.com>
2023-01-21 13:33:00 +0800

    Create utils for listening and sending ipc message

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,4 @@
 export { default as arrayLimit } from "./arrayLimit";
 export { default as format } from "./format";
+export { default as ipcRendererSendMsg } from "./ipcRendererSendMsg";
+export { default as ipcRendererListenerMsg } from "./ipcRendererListenerMsg";
